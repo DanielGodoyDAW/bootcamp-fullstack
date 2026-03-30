08abcb1fe5187db7e7df5c552344e743b82879c0
Northland <162111816+NorthlandWow@users.noreply.github.com>
2026-03-30 15:53:02 +0200

    chore: edit ejercicio09

--- js/js-arrays-objetos/Ejercicio09/ejercicio09.js
+++ js/js-arrays-objetos/Ejercicio09/ejercicio09.js
@@ -1,5 +1,49 @@
-function verResolucion(){
-    let resolucionAE = " ";
+function verResolucion() {
+  let resolucionAE = " ";
 
-    document.getElementById("resolucionAE").innerHTML = resolucionAE;
+  const cursos = [
+    {
+      titulo: "Introducción a JavaScript",
+      duracion: 40,
+      nivel: "principiante",
+      activo: true,
+    },
+    {
+      titulo: "JavaScript Avanzado",
+      duracion: 60,
+      nivel: "intermedio",
+      activo: true,
+    },
+    {
+      titulo: "React desde Cero",
+      duracion: 50,
+      nivel: "intermedio",
+      activo: true,
+    },
+    {
+      titulo: "Node.js y Express",
+      duracion: 55,
+      nivel: "avanzado",
+      activo: false,
+    },
+  ];
+
+  cursos.forEach((curso) => {
+    console.log(`
+        Titulo: ${curso.titulo }|
+        Duracion: ${curso.duracion} horas |
+        Nivel: ${curso.nivel} |
+        Activo: ${curso.activo}
+        `);
+  });
+
+  cursos.forEach((curso) => {
+    resolucionAE += `
+        Titulo: ${curso.titulo}|
+        Duracion: ${curso.duracion} horas|
+        Nivel: ${curso.nivel}|
+        Activo: ${curso.activo} <br>`;
+  });
+
+  document.getElementById("resolucionAE").innerHTML = resolucionAE;
 }
