71a8e094b6af82e513465aa85aac58c27f8153ed
Northland <162111816+NorthlandWow@users.noreply.github.com>
2026-04-02 13:59:24 +0200

    feat: add ejercicio17

--- js/js-arrays-objetos/Ejercicio17/ejercicio17.js
+++ js/js-arrays-objetos/Ejercicio17/ejercicio17.js
@@ -1,5 +1,21 @@
 function verSolucion(){
     let solucion = " ";
 
+    const usuario = {
+    nombre: "Juan",
+    edad: 30,
+    correo: "ejemplo@gmail.com"
+    };
+
+    const usuarioJSON = JSON.stringify(usuario);
+    console.log("Usuario en formato JSON:", usuarioJSON);
+    solucion += "Usuario en formato JSON: " + usuarioJSON + "<br>";
+
+    const usuarioObjeto = JSON.parse(usuarioJSON);
+    console.log("Usuario convertido de nuevo a objeto:", usuarioObjeto);
+    solucion += "Usuario convertido de nuevo a objeto: " + JSON.stringify(usuarioObjeto) + "<br>";
+    console.log("Nombre del usuario:", usuarioObjeto.nombre);
+    solucion += "Nombre del usuario: " + usuarioObjeto.nombre + "<br>";
+
     document.getElementById("solucion").innerHTML = solucion;
 }
