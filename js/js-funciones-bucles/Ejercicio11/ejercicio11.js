1b9d50a4f408d435002e12e56db469a182956cf0
Northland <162111816+NorthlandWow@users.noreply.github.com>
2026-04-02 15:28:42 +0200

    feat: edit ejer 11

--- js/js-funciones-bucles/Ejercicio11/ejercicio11.js
+++ js/js-funciones-bucles/Ejercicio11/ejercicio11.js
@@ -1,15 +1,23 @@
+const curso = "JavaScript";
+
+function mostrarCurso() {
+  const mensaje = `Estás en el curso de ${curso}`;
+  console.log(mensaje);
+  return mensaje;
+}
+
 function verResolucion() {
   let resolucionAE = " ";
+
+  resolucionAE += mostrarCurso() + "<br><br>";
+
+  if (true) {
+    const variableBloque = "Variable dentro del if";
+    console.log(variableBloque);
+    resolucionAE += variableBloque + "<br>";
+  }
 
   document.getElementById("resolucionAE").innerHTML = resolucionAE;
 }
 
-const curso = "JavaScript"; //esta varible esta definida para todo el archivo, por eso es vista desde dentro
-function mostrarCurso() {
-  const mensaje = `Estás en el curso de ${curso}`;
-
-  console.log(mensaje);
-}
-mostrarCurso();
-//este log no lo lee, porque la variable mensaje esta dentro de la funcion y desde fuera no es visible
-console.log(mensaje);
+verResolucion();
