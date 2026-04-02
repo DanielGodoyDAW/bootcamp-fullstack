828ffe2c461c5aa26daa44dab303726f7e6fff5a
Northland <162111816+NorthlandWow@users.noreply.github.com>
2026-04-02 15:16:16 +0200

    feat: edit ejercicio06

--- js/js-funciones-bucles/Ejercicio06/ejercicio06.js
+++ js/js-funciones-bucles/Ejercicio06/ejercicio06.js
@@ -1,19 +1,19 @@
-function verResolucion(){
-    let resolucionAE = " ";
+function verResolucion() {
+  let resolucionAE = " ";
 
-    
+  let resultado = suma(2, 3);
 
-    document.getElementById("resolucionAE").innerHTML = resolucionAE;
+  console.log(resultado);
+
+  let operacion = 4 - resultado;
+
+  console.log(operacion);
+
+  resolucionAE = `El resultado de la suma es ${resultado} y el resultado de la operación es ${operacion}`;
+
+  document.getElementById("resolucionAE").innerHTML = resolucionAE;
 }
 
-function suma (a, b){
-    return a + b;
+function suma(a, b) {
+  return a + b;
 }
-
-let resultado = suma(2,3);
-
-console.log(resultado);
-
-let operacion = 4 - resultado;
-
-console.log(operacion);
