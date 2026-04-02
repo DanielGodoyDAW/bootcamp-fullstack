42318ebbd88b18a0c8ae3c0bd7ddfba7741859f8
Northland <162111816+NorthlandWow@users.noreply.github.com>
2026-04-02 14:09:19 +0200

    feat: add ejercicio18

--- js/js-arrays-objetos/Ejercicio18/ejercicio18.js
+++ js/js-arrays-objetos/Ejercicio18/ejercicio18.js
@@ -1,5 +1,25 @@
 function verSolucion(){
     let solucion = " ";
 
+    const libros = [
+        {titulo: "El señor de los anillos", autor: "J.R.R. Tolkien", paginas: 1178},
+        {titulo: "Cien años de soledad", autor: "Gabriel García Márquez", paginas: 417},
+        {titulo: "Don Quijote de la Mancha", autor: "Miguel de Cervantes", paginas: 863},
+    ];
+
+    console.log("Librería original:", libros);
+    solucion += "Librería original: " + JSON.stringify(libros) + "<br><br>";
+
+    const libroJSON = JSON.stringify(libros);
+    console.log("Convertida a JSON:", libroJSON);
+    solucion += "Convertida a JSON: " + libroJSON + "<br><br>";
+
+    const libroObjeto = JSON.parse(libroJSON);
+    console.log("Reconstruida a objeto:", libroObjeto);
+    solucion += "Reconstruida a objeto: " + JSON.stringify(libroObjeto) + "<br><br>";
+
+    console.log("Primer elemento:", libroObjeto[0]);
+    solucion += "Primer elemento: " + JSON.stringify(libroObjeto[0]) + "<br>";
+
     document.getElementById("solucion").innerHTML = solucion;
 }
