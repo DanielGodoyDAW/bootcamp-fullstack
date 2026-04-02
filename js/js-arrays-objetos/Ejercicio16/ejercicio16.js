998245cedc5f02a74e5d5254ccee2341775e4671
Northland <162111816+NorthlandWow@users.noreply.github.com>
2026-04-02 13:55:01 +0200

    feat: add ejercicio16

--- js/js-arrays-objetos/Ejercicio16/ejercicio16.js
+++ js/js-arrays-objetos/Ejercicio16/ejercicio16.js
@@ -1,5 +1,29 @@
 function verSolucion(){
     let solucion = " ";
 
+    const valorNullo = null;
+    const valorIndefinido = undefined;
+    const valorVacio = "";
+
+    //con ??
+
+    console.log("Valor nulo con ?? :", valorNullo ?? "Valor por defecto");
+    console.log("Valor indefinido con ?? :", valorIndefinido ?? "Valor por defecto");
+    console.log("Valor vacío con ?? :", valorVacio ?? "Valor por defecto");
+
+    solucion += "Valor nulo con ?? : " + (valorNullo ?? "Valor por defecto") + "<br>";
+    solucion += "Valor indefinido con ?? : " + (valorIndefinido ?? "Valor por defecto") + "<br>";
+    solucion += "Valor vacío con ?? : " + (valorVacio ?? "Valor por defecto") + "<br>";
+
+    //con ||
+
+    console.log("Valor nulo con || :", valorNullo || "Valor por defecto");
+    console.log("Valor indefinido con || :", valorIndefinido || "Valor por defecto");
+    console.log("Valor vacío con || :", valorVacio || "Valor por defecto");
+
+    solucion += "Valor nulo con || : " + (valorNullo || "Valor por defecto") + "<br>";
+    solucion += "Valor indefinido con || : " + (valorIndefinido || "Valor por defecto") + "<br>";
+    solucion += "Valor vacío con || : " + (valorVacio || "Valor por defecto") + "<br>";
+
     document.getElementById("solucion").innerHTML = solucion;
 }
