4f834e0df2a2a01e533cf14b7b71d5ff4055fce2
Northland <162111816+NorthlandWow@users.noreply.github.com>
2026-04-10 19:02:56 +0200

    Feat: move files

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,15 +1,15 @@
-import Separador from './Separador.jsx';
-import WelcomeCard from "./Ejercicio02/WelcomeCard.jsx";
-import Appi from "./Ejercicio03/Appi.jsx";
-import Header from './Ejercicio04/Header.jsx';
-import Subtitle from './Ejercicio04/Subtittle.jsx';
-import InfoBox from './Ejercicio04/InfoBox.jsx';
-import Footer from './Ejercicio04/Footer.jsx';
-import AppEje5 from './Ejercicio05/AppEje5.jsx';
-import App7 from './Ejercicio07/App7.jsx';
-import App9 from './Ejercicio09/App9.jsx';
-import App11 from './Ejercicio11/App11.jsx';
-import App12 from './Ejercicio12/App12.jsx';
+import Separador from './Introduccion/Separador.jsx';
+import WelcomeCard from "./Introduccion/Ejercicio02/WelcomeCard.jsx";
+import Appi from "./Introduccion/Ejercicio03/Appi.jsx";
+import Header from './Introduccion/Ejercicio04/Header.jsx';
+import Subtitle from './Introduccion/Ejercicio04/Subtittle.jsx';
+import InfoBox from './Introduccion/Ejercicio04/InfoBox.jsx';
+import Footer from './Introduccion/Ejercicio04/Footer.jsx';
+import AppEje5 from './Introduccion/Ejercicio05/AppEje5.jsx';
+import App7 from './Introduccion/Ejercicio07/App7.jsx';
+import App9 from './Introduccion/Ejercicio09/App9.jsx';
+import App11 from './Introduccion/Ejercicio11/App11.jsx';
+import App12 from './Introduccion/Ejercicio12/App12.jsx';
 
 export default function App() {
   return (
