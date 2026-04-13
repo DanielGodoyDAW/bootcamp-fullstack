08a3857fd6153467208e23fc5975e24927966df1
NorthlandWow <d4n1.g0d0y@gmail.com>
2026-04-13 13:11:59 +0200

    Feat: add ejercicio05

--- react/react-ts/src/App.tsx
+++ react/react-ts/src/App.tsx
@@ -2,30 +2,88 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from './assets/vite.svg'
 // import heroImg from './assets/hero.png'
-import './App.css'
-import CourseCard from './Props-composicion/Ejercicio02/CourseCard'
-import Separador from './Props-composicion/Separador'
-import ProfileCard from './Props-composicion/Ejercicio03/ProfileCard'
-import TechCard from './Props-composicion/Ejercicio04/TechCard'
+import "./App.css";
+import CourseCard from "./Props-composicion/Ejercicio02/CourseCard";
+import Separador from "./Props-composicion/Separador";
+import ProfileCard from "./Props-composicion/Ejercicio03/ProfileCard";
+import TechCard from "./Props-composicion/Ejercicio04/TechCard";
+import ModuleCard from "./Props-composicion/Ejercicio05/ModuleCard";
 
 function App() {
+  const modules = [
+    {
+      name: "HTML & CSS",
+      sessions: 8,
+      level: "Principiante",
+      description: "Fundamentos de la web",
+    },
+    {
+      name: "JavaScript",
+      sessions: 12,
+      level: "Principiante",
+      description: "Lenguaje de la web",
+    },
+    {
+      name: "React",
+      sessions: 16,
+      level: "Intermedio",
+      description: "Librería para UI",
+    },
+    {
+      name: "Node.js",
+      sessions: 14,
+      level: "Intermedio",
+      description: "Backend con JavaScript",
+    },
+    {
+      name: "Bases de datos",
+      sessions: 10,
+      level: "Intermedio",
+      description: "SQL y NoSQL",
+    },
+  ];
 
   return (
     <>
-      <Separador/>
+      <Separador />
       <h2>Ejercicio 2</h2>
-      <CourseCard title='React desde cero' level='Intermedio' duration='6 semanas' textButton='Ver curso' />
-      <Separador/>
+      <CourseCard
+        title="React desde cero"
+        level="Intermedio"
+        duration="6 semanas"
+        textButton="Ver curso"
+      />
+      <Separador />
       <h2>Ejercicio 3</h2>
       <ProfileCard name="Lucía" role="Frontend Developer" city="Madrid" />
-      <Separador/>
+      <Separador />
       <ProfileCard name="Álvaro" role="Backend Developer" city="Sevilla" />
       <Separador />
       <h2>Ejercicio 4</h2>
-      <TechCard name='titulo 1' category='categoria 1' description='descripcion 1'/>
-      <TechCard name='titulo 2' category='categoria 2' description='descripcion 2'/>
+      <TechCard
+        name="titulo 1"
+        category="categoria 1"
+        description="descripcion 1"
+      />
+      <TechCard
+        name="titulo 2"
+        category="categoria 2"
+        description="descripcion 2"
+      />
+      <h2>Ejercicio 5</h2>
+      <div>
+        {modules.map((module, index) => (
+        <ModuleCard
+          key={index}  
+          name={module.name}
+          session={module.sessions}
+          level={module.level}
+          description={module.description}
+        />
+      ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
