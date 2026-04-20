97e4be91946a69946062e1422c463114dac1a6a3
Northland <162111816+NorthlandWow@users.noreply.github.com>
2026-04-20 18:08:10 +0200

    Feat: primeros pasos de transicion a react de la poekApi

--- fronted/pokedex/src/components/PokemonCard.tsx
+++ fronted/pokedex/src/components/PokemonCard.tsx
@@ -1,26 +1,40 @@
-function PokemonCard(id, name, types, image, gif, evolution) {
+function PokemonCard({ pokemon }) {
+  const nombre = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+  const imagen = pokemon.sprites.front_default;
+  const gif =
+    pokemon.sprites.versions["generation-v"]["black-white"].animated
+      .front_default;
+  const tipoPrincipal = pokemon.types?.[0]?.type?.name ?? "normal";
+
   return (
     <>
-       <article className="container container-planta">
-        <div className="container-div container-div-planta">
+      <article className={`container container-${tipoPrincipal}`}>
+        <div className={`container-div container-div-${tipoPrincipal}`}>
           <figure className="pokemon-img">
-            <img className="static" src="" alt="imagen de Bulbasaur" />
-            <img className="gif" src="" alt="imagen de Bulbasaur" />
+            <img className="static" src={imagen} alt={`imagen de ${nombre}`} />
+            <img className="gif" src={gif} alt={`imagen de ${nombre}`} />
           </figure>
-          <p className="container-div-p-ID">ID: {id}</p>
+          <p className="container-div-p-ID">ID: {pokemon.id}</p>
         </div>
         <div>
           <p>
-            <strong className={types}>{name}</strong>
+            <strong className={tipoPrincipal}>{nombre}</strong>
           </p>
           <p>
-            <span className="tipo veneno">Poison</span>
-            <span className="tipo hierva">Grass</span>
+            {pokemon.types.map((item) => (
+              <span className={`tipo ${item.type.name}`} key={item.slot}>
+                {item.type.name.charAt(0).toUpperCase() + item.type.name.slice(1)}
+              </span>
+            ))}
           </p>
+          {pokemon.evolution && (
+            <div className="evolution">
+            Evoluciona de <span className={`evolution-pokemon evolution-${tipoPrincipal}`}>{pokemon.evolution}</span>
+          </div>)}
         </div>
       </article>
     </>
-  )
+  );
 }
 
-export default PokemonCard
+export default PokemonCard;
---
+++ fronted/pokedex/src/services/pokemonApi.ts
@@ -0,0 +1,136 @@
+function buscarEvolucion(chain, nombrePokemon, evolucionAnterior = null) {
+  // Si encontramos el pokemon actual
+  if (chain.species.name === nombrePokemon) {
+    if (evolucionAnterior) {
+      return (
+        evolucionAnterior.charAt(0).toUpperCase() + evolucionAnterior.slice(1)
+      );
+    }
+    return null;
+  }
+
+  // Si no es el pokemon actual, buscamos en las evoluciones posteriores
+  for (let evolucion of chain.evolves_to) {
+    const resultado = buscarEvolucion(
+      evolucion,
+      nombrePokemon,
+      chain.species.name,
+    );
+    if (resultado !== undefined) {
+      return resultado;
+    }
+  }
+
+  return undefined;
+}
+
+async function obtenerEvolucion(pokemonId) {
+  try {
+    // Obtener los datos de la especie
+    const responseSpecies = await fetch(
+      `https://pokeapi.co/api/v2/pokemon-species/${pokemonId}/`,
+    );
+    const datosSpecies = await responseSpecies.json();
+
+    // Obtener la cadena de evolución
+    const urlEvolutionChain = datosSpecies.evolution_chain.url;
+    const responseEvolutionChain = await fetch(urlEvolutionChain);
+    const datosEvolutionChain = await responseEvolutionChain.json();
+
+    // Buscar el pokemon actual en la cadena y obtener su evolución
+    const evolucionEncontrada = buscarEvolucion(
+      datosEvolutionChain.chain,
+      datosSpecies.name,
+    );
+
+    return evolucionEncontrada;
+  } catch (error) {
+    console.error("Error al obtener evolución:", error);
+    return null;
+  }
+}
+
+async function formatearPokemon(datosApi) {
+
+    // aprovechamos el tipo original
+    const mainType = datosApi.types?.[0]?.type?.name ?? "normal"
+
+    // Obtener la evolución
+    const evolucion = await obtenerEvolucion(datosApi.id);
+
+    // GIF con fallback a imagen estática
+    const gifUrl = datosApi.sprites.versions?.["generation-v"]?.["black-white"]?.animated?.front_default;
+    const imagenFinal = gifUrl || datosApi.sprites.front_default;
+
+
+    return{
+      id: datosApi.id,
+      name:
+        datosApi.name.charAt(0).toUpperCase() + datosApi.name.slice(1),
+      types: datosApi.types.map(
+        (tipo) => tipo.type.name.charAt(0).toUpperCase() + tipo.type.name.slice(1),
+      ),
+      image: datosApi.sprites.front_default,
+      gif: imagenFinal,
+      type_color: mainType,
+      evolution: evolucion,
+    };
+    
+}
+
+async function cargarPokemons() {
+  try {
+    // Obtener la lista de pokemons
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=9`);
+    const datos = await response.json();
+
+    // Usar Promise.all para esperar a que se resuelvan todas las promesas de los pokemons
+    const pokemons = await Promise.all(
+      datos.results.map(async (pokemonLista) => {
+        const respuesta = await fetch(pokemonLista.url);
+        const datosAPI = await respuesta.json();
+
+        return formatearPokemon(datosAPI);
+      }),
+    );
+
+    return pokemons;
+  } catch (error) {
+    console.error("Error:", error);
+    return [];
+  }
+}
+
+export { cargarPokemons, formatearPokemon };
+
+// async function realizarBusqueda(nombre) {
+//   try {
+//     const pokemonNombre = nombre.target.value.trim().toLowerCase();
+//     if (!pokemonNombre) {
+//       renderizado(pokemonsBase);
+//       return;
+//     }
+
+//     // Si son SOLO números -> no permitir
+//     if (/^\d+$/.test(pokemonNombre)) {
+//       // aquí puedes mostrar un aviso o simplemente volver a la lista
+//       console.warn("La búsqueda no puede contener solo números.");
+//       // mostramos las tarjetas base
+//       renderizado(pokemonsBase);
+//       return;
+//     }
+//     const responseBusqueda = await fetch(
+//       `https://pokeapi.co/api/v2/pokemon/${pokemonNombre}`,
+//     );
+//     if (!responseBusqueda.ok) {
+//       return;
+//     }
+//     const dataBusqueda = await responseBusqueda.json();
+//     //console.log(dataBusqueda);
+
+//     const pokemonRenderizado = await formatearPokemon(dataBusqueda);
+//     renderizado([pokemonRenderizado]);
+//   } catch (error) {
+//     console.error("Error:", error);
+//   }
+// }
