366e5670946c99d47cac8fbad8e31e61cbe6b7cb
NorthlandWow <d4n1.g0d0y@gmail.com>
2026-04-21 08:52:49 +0200

    Feat: modify pokeapi react

--- fronted/pokedex/src/components/PokemonCard.tsx
+++ fronted/pokedex/src/components/PokemonCard.tsx
@@ -1,35 +1,35 @@
 function PokemonCard({ pokemon }) {
-  const nombre = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
-  const imagen = pokemon.sprites.front_default;
-  const gif =
-    pokemon.sprites.versions["generation-v"]["black-white"].animated
-      .front_default;
-  const tipoPrincipal = pokemon.types?.[0]?.type?.name ?? "normal";
+  // const nombre = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+  // const imagen = pokemon.sprites.front_default;
+  // const gif =
+  //   pokemon.sprites.versions["generation-v"]["black-white"].animated
+  //     .front_default;
+  // const tipoPrincipal = pokemon.types?.[0]?.type?.name ?? "normal";
 
   return (
     <>
-      <article className={`container container-${tipoPrincipal}`}>
-        <div className={`container-div container-div-${tipoPrincipal}`}>
+      <article className={`container container-${pokemon.type_color}`}>
+        <div className={`container-div container-div-${pokemon.type_color}`}>
           <figure className="pokemon-img">
-            <img className="static" src={imagen} alt={`imagen de ${nombre}`} />
-            <img className="gif" src={gif} alt={`imagen de ${nombre}`} />
+            <img className="static" src={pokemon.image} alt={`imagen de ${pokemon.name}`} />
+            <img className="gif" src={pokemon.gif} alt={`imagen de ${pokemon.name}`} />
           </figure>
           <p className="container-div-p-ID">ID: {pokemon.id}</p>
         </div>
         <div>
           <p>
-            <strong className={tipoPrincipal}>{nombre}</strong>
+            <strong className={pokemon.types_color}>{pokemon.name}</strong>
           </p>
           <p>
             {pokemon.types.map((item) => (
-              <span className={`tipo ${item.type.name}`} key={item.slot}>
-                {item.type.name.charAt(0).toUpperCase() + item.type.name.slice(1)}
+              <span className={`tipo ${item}`} key={item}>
+                {item}
               </span>
             ))}
           </p>
           {pokemon.evolution && (
             <div className="evolution">
-            Evoluciona de <span className={`evolution-pokemon evolution-${tipoPrincipal}`}>{pokemon.evolution}</span>
+            Evoluciona de <span className={`evolution-pokemon evolution-${pokemon.type_color}`}>{pokemon.evolution}</span>
           </div>)}
         </div>
       </article>
