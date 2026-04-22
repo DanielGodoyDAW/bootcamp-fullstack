02943daab899677d43818a6749d9d1861b8f1edd
Northland <162111816+NorthlandWow@users.noreply.github.com>
2026-04-22 16:50:50 +0200

    Feat: edit pop up

--- fronted/pokedex/src/components/PokemonPopUp.tsx
+++ fronted/pokedex/src/components/PokemonPopUp.tsx
@@ -18,13 +18,32 @@ interface PokemonPopupProps {
 
 function PokemonPopUp({ pokemon, visible, onClose }: PokemonPopupProps) {
    if (!visible || !pokemon) return null;
+
+   // Obtén los colores CSS de los tipos
+  const color1 = `var(--color-${pokemon.types[0].toLowerCase()})`;
+  const color2 = pokemon.types[1] 
+    ? `var(--color-${pokemon.types[1].toLowerCase()})`
+    : color1;
+
+    // Aplica el gradiente
+  const gradientStyle = {
+    background: `linear-gradient(135deg, ${color1}, ${color2})`,
+  };
   return (
     <div className="popup-overlay" onClick={onClose}>
-      <div className="popup-content" onClick={e => e.stopPropagation()}>
+      <div className="popup-content" style={gradientStyle} onClick={e => e.stopPropagation()}>
         <h2>{pokemon.name}</h2>
         <img src={pokemon.image} alt={pokemon.name} />
-        <p>Tipos: {pokemon.types.join(", ")}</p>
-        {/* Aquí más info o evolución, etc */}
+        <p>{pokemon.types.map((item) => (
+              <span className={`tipo ${item}`} key={item}>
+                {item.charAt(0).toUpperCase() + item.slice(1)}
+              </span>
+            ))}</p>
+            {pokemon.evolution && (
+              <div className="evolution">
+              <span className="evolution-text">Evoluciona de </span>
+              <span className={`evolution-pokemon`}>{pokemon.evolution}</span>
+          </div>)}
         <button onClick={onClose}>Cerrar</button>
       </div>
     </div>
